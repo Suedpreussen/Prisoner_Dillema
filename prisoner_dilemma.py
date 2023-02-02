ea43d606b1189bb7fd2beb664c0b69ae2a29e211
Suedpreussen <piotr1969.fi@gmail.com>
2023-02-02 19:46:29 +0100

    docs and log function for simplest_dilemma.py | began with new, generalised version prisoner_dilemma.py

---
+++ prisoner_dilemma.py
@@ -0,0 +1,20 @@
+class Strategy:
+    pass
+
+
+class Player:
+    def __init__(self, points):
+        points = points
+
+
+def prisoner_dilemma(player_a: Player, player_b: Player, strategy: Strategy):
+    pass
+
+
+"""
+my_strategy = Strategy()
+player_A = Player()
+player_B = Player()
+
+prisoner_dilemma(player_A, player_B, my_strategy)
+"""
